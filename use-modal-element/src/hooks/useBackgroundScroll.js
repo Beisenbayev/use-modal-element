4edd5474f2419baabf9338010a7a9a0dbac7768c
beisenbayev <beisenbayeff.nursultan@gmail.com>
2022-01-30 12:08:20 +0600

    scrollable background was fixed

--- use-modal-element/src/hooks/useBackgroundScroll.js
+++ use-modal-element/src/hooks/useBackgroundScroll.js
@@ -1,16 +1,27 @@
 import { useEffect } from "react"
+import s from '../styles/index.module.css';
 
 const useBackgroundScroll = (opened, condition) => {
    useEffect(() => {
       const bodyElement = document.querySelector('body');
+      const htlmElement = document.querySelector('html');
 
       if (!condition) {
-         if (opened) bodyElement.classList.add('overflow-hidden');
-         else bodyElement.classList.remove('overflow-hidden');
+         if (opened) {
+            bodyElement.classList.add(s.overflowHidden);
+            htlmElement.classList.add(s.overflowHidden);
+         }
+         else {
+            bodyElement.classList.remove(s.overflowHidden);
+            htlmElement.classList.remove(s.overflowHidden);
+         }
       }
 
       return () => {
-         bodyElement.classList.remove('overflow-hidden')
+         {
+            bodyElement.classList.remove(s.overflowHidden);
+            htlmElement.classList.remove(s.overflowHidden);
+         }
       }
 
    }, [opened, condition])
