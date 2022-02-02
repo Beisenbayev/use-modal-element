5ae764e9f7668e83939d504d539695ea1313f0ef
beisenbayev <beisenbayeff.nursultan@gmail.com>
2022-02-02 22:34:19 +0600

    npm error fix

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,11 @@ module.exports = {
    },
    module: {
       rules: [
-         { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+         {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: "babel-loader"
+         },
          {
             test: /\.css$/i,
             use: ['style-loader', 'css-loader'],
